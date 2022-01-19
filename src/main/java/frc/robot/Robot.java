54d064b5cb0c42b51d6f08e6cb3d7a646b98b81b
Gurvir Singh <charizardzx9@gmail.com>
2022-01-18 19:56:52 -0500

    Setup drive motors and got the drive working

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,98 +1,92 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.motorcontrol.*;
+
+
+/*
+ * Your best friend: https://first.wpi.edu/FRC/roborio/release/docs/java/
+ * the link above is the documentation for the classes and library we use
+ *
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ *
+ * TimedRobot: It provides control of the robot program through a collection of init() and periodic() methods,
+ * which are called by WPILib during specific robot states (e.g. autonomous or teleoperated).
+ * By default, periodic methods are called every 20ms; this can be changed by overriding the getPeriod() method.
+ * 
+ * Any method with @Overide above it is a function in the class TimedRobot which is what our code inherits from. We override them with
+ * the functionality we need from out robot and shouldnt be deleted. Deleting them wont do anything and you can reimplment them later
+ * but I recommend not removing them for simplicity. The other methods are ones we defined completly for ourselves and I prefer to put
+ * them near the top of the code. These are used to simplify code and make code snippets reusable. An example being the Launcher()
+ * method that should set the launcher to the speed you pass in and make sures they spin in oppisite directions. That way we just need
+ * to call Luancher when we need it instead of rewriting it everytime we need to set the speed of the launcher. 
+ * 
+ * Here is the explanation for all the methods we overide from the TimedRobot class:
+ * Note: If you want more info on any of these or see the other ones we havent implemented you can find it in the documentation below: 
+ * https://first.wpi.edu/FRC/roborio/release/docs/java/
+ * 
+ * robotInit() is the function run when the robot starts up. Use this for code you need to run when the robot is enabled
+ * 
+ * teleopInit() is the function run when the robot starts into tele-operated mode (when you control the robot remotly).
+ * 
+ * teleopPeriodic() is the function run consantly when in tele-operated mode. A very simplified explanation is while the robot is in 
+ * tele-operated mode it will do the instructions in this function every 20ms. So this is where stuff like your drive and shooting go
+ * 
+ * autonomousInit() is the function run when the robot starts into autonomous mode (when the robot moves on its own).
+ * 
+ * autonomousPeriodic() is the function run consantly when in autonomous mode. A very simplified explanation is while the robot is in 
+ * autonomous mode it will do the instructions in this function every 20ms. So this is where stuff like drive and shooting go
  */
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+
+  private final PWMVictorSPX LeftDriveMotor1 = new PWMVictorSPX(0); 
+  private final PWMVictorSPX LeftDriveMotor2 = new PWMVictorSPX(1);  
+  private final PWMVictorSPX RightDriveMotor1 = new PWMVictorSPX(2); 
+  private final PWMVictorSPX RightDriveMotor2 = new PWMVictorSPX(3);
+  private final MotorControllerGroup LeftMotors = new MotorControllerGroup(LeftDriveMotor1, LeftDriveMotor2);
+  private final MotorControllerGroup RightMotors = new MotorControllerGroup(RightDriveMotor1, RightDriveMotor2);
+  private final DifferentialDrive RobotDrive = new DifferentialDrive(LeftMotors,RightMotors);
+  private final Joystick Joystick1 = new Joystick(0);
+  //intake, shooter, transfer
   @Override
   public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
+    
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
   @Override
-  public void robotPeriodic() {}
+  public void teleopInit() {
+    
+    
+  }
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
+  @Override
+  public void teleopPeriodic() {
+    RobotDrive.arcadeDrive((Joystick1.getY()), -(Joystick1.getX()));
+  }
+  
   @Override
   public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
+    
   }
 
-  /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
+    
   }
-
-  /** This function is called once when teleop is enabled. */
-  @Override
-  public void teleopInit() {}
-
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {}
-
-  /** This function is called once when the robot is disabled. */
-  @Override
-  public void disabledInit() {}
-
-  /** This function is called periodically when disabled. */
-  @Override
-  public void disabledPeriodic() {}
-
-  /** This function is called once when test mode is enabled. */
-  @Override
-  public void testInit() {}
-
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
+  
 }
